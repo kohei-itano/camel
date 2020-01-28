477100bebca35f1947ce61db80e68d76ccc61a4a
kohei-itano <itanokohei33@gmail.com>
2020-01-28 04:37:17 +0000

    finish

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,2 +1,3 @@
 class Admin::AdminsController < ApplicationController
+
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_01_12_074304) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
