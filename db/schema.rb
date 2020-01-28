2b0816c369933f0b23295bbcd4df4ed76a910c56
kohei-itano <itanokohei33@gmail.com>
2020-01-28 06:55:47 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_01_12_074304) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", null: false
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
